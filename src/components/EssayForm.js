8e578129347b811bd93c2ef27a311ee683e3b160
Pearl Zeng <zhuzeng@umich.edu>
2017-06-04 12:58:33 -0400

    Work in progress: set the form to accept multiple text inputs.
    Should I consider map function?

--- src/components/EssayForm.js
+++ src/components/EssayForm.js
@@ -3,15 +3,23 @@ import React from 'react';
 class EssayForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: 'What is your favorite color?'};
+    this.state = {
+      name: 'your name',
+      color: 'What is your favorite color?'
+    };
   }
 
   handleChange = (event) => {
-    this.setState({value: event.target.value});
+    const target = event.target;
+    const name = target.name;
+    this.setState({
+      name: target.value,
+      color: target.value
+    });
   }
 
   handleSubmit = (event) => {
-    alert('Your favorite color is ' + this.state.value + '.');
+    alert(this.state.name + '’s favorite color is ' + this.state.color + '.');
     event.preventDafualt();
   }
 
@@ -19,8 +27,12 @@ class EssayForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
+          Your name:
+          <input type="text" value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <label>
           Color:
-          <textarea value={this.state.value} onChange={this.handleChange} />
+          <textarea value={this.state.color} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
