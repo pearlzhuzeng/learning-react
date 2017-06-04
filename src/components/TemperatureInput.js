9d358d86db444d8c767e4e2210a66196d8918e0b
Pearl Zeng <zhuzeng@umich.edu>
2017-06-04 17:31:30 -0400

    Adds TemperatureInput and hooks it up with BoilingCalculator.
    'In React, sharing state is accomplished by moving it up to the closest common ancestor of the components that need it. This is called "lifting state up".'

--- src/components/TemperatureInput.js
+++ src/components/TemperatureInput.js
@@ -3,26 +3,27 @@ import React from 'react';
 class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {temperature: ''};
   }
 
   handleChange = (e) => {
-    this.setState({temperature: e.target.value});
+    //Before: this.setState({temperature: e.target.value});
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    //Before: const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Enter temperature in {scaleName[scale]}:</legend>
+        <legend>Enter temperature in {scaleNames[scale]}: </legend>
         <input value={temperature} onChange={this.handleChange} />
       </fieldset>
     );
   }
 }
 
-const scaleName = {
+const scaleNames = {
   c: 'Celsius',
   f: 'Fahrenheit'
 };
