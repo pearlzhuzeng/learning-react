844f06e596a667a0d3986c715efefc3760485f75
Pearl Zeng <zhuzeng@umich.edu>
2017-06-04 12:09:48 -0400

    Adds ReservationForm.
    Handling multiple inputs.

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NameForm from './NameForm';
 import EssayForm from './EssayForm';
 import FlavorForm from './FlavorForm';
+import ReservationForm from './ReservationForm';
 
 class Forms extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class Forms extends React.Component {
         <div><NameForm /></div>
         <div><EssayForm /></div>
         <div><FlavorForm /></div>
+        <div><ReservationForm /></div>
       </div>
     );
   }
