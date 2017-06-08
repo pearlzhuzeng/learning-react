e1d9a57590440e6bd1b1a34e097f57a141c874dd
Pearl Zeng <zhuzeng@umich.edu>
2017-06-08 08:45:00 -0400

    Adds TodoListThree, saved with flow rules.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './components/Welcome';
-import Comment from './components/Comment';
-import Clock from './components/Clock';
-import Counter from './components/Counter';
-import ToggleOnOff from './components/ToggleOnOff';
-import LoginControl from './components/LoginControl';
-import Mailbox from './components/Mailbox';
-import PageWarning from './components/PageWarning';
-import NumberList from './components/NumberList';
-import Forms from './components/Forms';
-import BoilingCalculator from './components/BoilingCalculator';
-import FancyBorder from './components/FancyBorder';
-import SplitPane from './components/SplitPane';
-import FilterableProductTable from './components/FilterableProductTable';
-import TodoList from './components/TodoList';
-import TodoListTwo from './components/TodoListTwo';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Welcome from './components/Welcome'
+import Comment from './components/Comment'
+import Clock from './components/Clock'
+import Counter from './components/Counter'
+import ToggleOnOff from './components/ToggleOnOff'
+import LoginControl from './components/LoginControl'
+import Mailbox from './components/Mailbox'
+import PageWarning from './components/PageWarning'
+import NumberList from './components/NumberList'
+import Forms from './components/Forms'
+import BoilingCalculator from './components/BoilingCalculator'
+import FancyBorder from './components/FancyBorder'
+import SplitPane from './components/SplitPane'
+import FilterableProductTable from './components/FilterableProductTable'
+import TodoList from './components/TodoList'
+import TodoListTwo from './components/TodoListTwo'
+import TodoListThree from './components/TodoListThree'
 
 const comment = {
   date: new Date(),
   text: 'This is the first comment',
   author: {
     name: 'Pearl Zeng',
-    avatarUrl: 'https://i1.wp.com/asknature.org/wp-content/uploads/strategy/a284195e8d52b20f9ac84213545b96f1/squirrel.jpg?resize=646%2C430&ssl=1'
-  }
+    avatarUrl:
+      'https://i1.wp.com/asknature.org/wp-content/uploads/strategy/a284195e8d52b20f9ac84213545b96f1/squirrel.jpg?resize=646%2C430&ssl=1',
+  },
 }
 
 const mailuser = {
   name: 'Squirrel',
-  messages: ['message 1', 'message 2', 'message 3', 'message 4']
+  messages: ['message 1', 'message 2', 'message 3', 'message 4'],
 }
 
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5]
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <div className="App-header">
@@ -58,9 +60,14 @@ class App extends Component {
         <div><Counter /></div>
         <div><ToggleOnOff /></div>
         <div><LoginControl /></div>
-        <div><Mailbox unreadMessages={mailuser.messages} userName={mailuser.name} /></div>
+        <div>
+          <Mailbox
+            unreadMessages={mailuser.messages}
+            userName={mailuser.name}
+          />
+        </div>
         <div><PageWarning /></div>
-        <div><NumberList numbers={numbers}/></div>
+        <div><NumberList numbers={numbers} /></div>
         <div><Forms /></div>
         <div><BoilingCalculator /></div>
         <div><FancyBorder /></div>
@@ -68,9 +75,10 @@ class App extends Component {
         <div><FilterableProductTable /></div>
         <div><TodoList /></div>
         <div><TodoListTwo /></div>
+        <div><TodoListThree /></div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
